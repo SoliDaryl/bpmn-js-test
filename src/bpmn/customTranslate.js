ac7636bfc0929492360263f15171551d8e469f71
solidaryl <solidaryl@outlook.com>
2019-06-26 19:16:40 +0800

    添加汉化

---
+++ src/bpmn/customTranslate.js
@@ -0,0 +1,29 @@
+import zh_cn from './zh_cn'
+
+export default function customTranslate(template, replacements) {
+  debugger
+  console.log("template<<>>" + template)
+  replacements = replacements || {};
+
+  // Translate
+  template = zh_cn[template] || template;
+
+  // Replace
+  return template.replace(/{([^}]+)}/g, function(_, key) {
+    return replacements[key] || '{' + key + '}';
+  });
+}
+
+
+// export const customTranslate = (template, replacements) => {
+//   console.log("template<<>>" + template)
+//   replacements = replacements || {};
+//
+//   // Translate
+//   template = zh_cn[template] || template;
+//
+//   // Replace
+//   return template.replace(/{([^}]+)}/g, function(_, key) {
+//     return replacements[key] || '{' + key + '}';
+//   });
+// }
