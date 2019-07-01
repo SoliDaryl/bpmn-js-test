060d67e45ecc25c3190129ceece97993970009a2
solidaryl <solidaryl@outlook.com>
2019-07-01 09:14:59 +0800

    修改创建模板“

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,70 +44,26 @@
         },
         methods: {
             createNewDiagram() {
-                const bpmnXmlStr = '<?xml version="1.0" encoding="UTF-8"?>\n' +
-                    '<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_0fppxr8" targetNamespace="http://bpmn.io/schema/bpmn">\n' +
-                    '  <bpmn:process id="Process_1" isExecutable="false">\n' +
-                    '    <bpmn:startEvent id="StartEvent_1" name="begin&#10;">\n' +
-                    '      <bpmn:outgoing>SequenceFlow_0nrfbee</bpmn:outgoing>\n' +
-                    '    </bpmn:startEvent>\n' +
-                    '    <bpmn:task id="Task_0ho18x0" name="hello&#10;">\n' +
-                    '      <bpmn:incoming>SequenceFlow_0nrfbee</bpmn:incoming>\n' +
-                    '      <bpmn:outgoing>SequenceFlow_00ho26x</bpmn:outgoing>\n' +
-                    '    </bpmn:task>\n' +
-                    '    <bpmn:task id="Task_1ymuvem" name="world">\n' +
-                    '      <bpmn:incoming>SequenceFlow_00ho26x</bpmn:incoming>\n' +
-                    '      <bpmn:outgoing>SequenceFlow_18df8vb</bpmn:outgoing>\n' +
-                    '    </bpmn:task>\n' +
-                    '    <bpmn:endEvent id="EndEvent_id" name="end">\n' +
-                    '      <bpmn:incoming>SequenceFlow_18df8vb</bpmn:incoming>\n' +
-                    '    </bpmn:endEvent>\n' +
-                    '    <bpmn:sequenceFlow id="SequenceFlow_0nrfbee" sourceRef="StartEvent_1" targetRef="Task_0ho18x0" />\n' +
-                    '    <bpmn:sequenceFlow id="SequenceFlow_00ho26x" sourceRef="Task_0ho18x0" targetRef="Task_1ymuvem" />\n' +
-                    '    <bpmn:sequenceFlow id="SequenceFlow_18df8vb" sourceRef="Task_1ymuvem" targetRef="EndEvent_id" />\n' +
-                    '  </bpmn:process>\n' +
-                    '  <bpmndi:BPMNDiagram id="BPMNDiagram_1">\n' +
-                    '    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">\n' +
-                    '      <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">\n' +
-                    '        <dc:Bounds x="173" y="102" width="36" height="36" />\n' +
-                    '        <bpmndi:BPMNLabel>\n' +
-                    '          <dc:Bounds x="178" y="145" width="27" height="27" />\n' +
-                    '        </bpmndi:BPMNLabel>\n' +
-                    '      </bpmndi:BPMNShape>\n' +
-                    '      <bpmndi:BPMNShape id="Task_0ho18x0_di" bpmnElement="Task_0ho18x0">\n' +
-                    '        <dc:Bounds x="485" y="244" width="100" height="80" />\n' +
-                    '      </bpmndi:BPMNShape>\n' +
-                    '      <bpmndi:BPMNShape id="Task_1ymuvem_di" bpmnElement="Task_1ymuvem">\n' +
-                    '        <dc:Bounds x="712" y="391" width="100" height="80" />\n' +
-                    '      </bpmndi:BPMNShape>\n' +
-                    '      <bpmndi:BPMNShape id="EndEvent_id_di" bpmnElement="EndEvent_id">\n' +
-                    '        <dc:Bounds x="1056" y="568" width="36" height="36" />\n' +
-                    '        <bpmndi:BPMNLabel>\n' +
-                    '          <dc:Bounds x="1065" y="611" width="19" height="14" />\n' +
-                    '        </bpmndi:BPMNLabel>\n' +
-                    '      </bpmndi:BPMNShape>\n' +
-                    '      <bpmndi:BPMNEdge id="SequenceFlow_0nrfbee_di" bpmnElement="SequenceFlow_0nrfbee">\n' +
-                    '        <di:waypoint x="209" y="120" />\n' +
-                    '        <di:waypoint x="347" y="120" />\n' +
-                    '        <di:waypoint x="347" y="284" />\n' +
-                    '        <di:waypoint x="485" y="284" />\n' +
-                    '      </bpmndi:BPMNEdge>\n' +
-                    '      <bpmndi:BPMNEdge id="SequenceFlow_00ho26x_di" bpmnElement="SequenceFlow_00ho26x">\n' +
-                    '        <di:waypoint x="585" y="284" />\n' +
-                    '        <di:waypoint x="649" y="284" />\n' +
-                    '        <di:waypoint x="649" y="431" />\n' +
-                    '        <di:waypoint x="712" y="431" />\n' +
-                    '      </bpmndi:BPMNEdge>\n' +
-                    '      <bpmndi:BPMNEdge id="SequenceFlow_18df8vb_di" bpmnElement="SequenceFlow_18df8vb">\n' +
-                    '        <di:waypoint x="812" y="431" />\n' +
-                    '        <di:waypoint x="934" y="431" />\n' +
-                    '        <di:waypoint x="934" y="586" />\n' +
-                    '        <di:waypoint x="1056" y="586" />\n' +
-                    '      </bpmndi:BPMNEdge>\n' +
-                    '    </bpmndi:BPMNPlane>\n' +
-                    '  </bpmndi:BPMNDiagram>\n' +
-                    '</bpmn:definitions>\n'
-                // 将字符串转换成图显示出来
+                const bpmnXmlStr = '<?xml version="1.0" encoding="UTF-8"?>' +
+                                    '<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+                                                    'xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" ' +
+                                                    'xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" ' +
+                                                    'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" ' +
+                                                    'targetNamespace="http://bpmn.io/schema/bpmn" ' +
+                                                    'id="Definitions_1">' +
+                                        '<bpmn:process id="Process_1" isExecutable="false">' +
+                                            '<bpmn:startEvent id="StartEvent_1"/>' +
+                                        '</bpmn:process>' +
+                                        '<bpmndi:BPMNDiagram id="BPMNDiagram_1">' +
+                                            '<bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">' +
+                                                '<bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">' +
+                                                    '<dc:Bounds height="36.0" width="36.0" x="173.0" y="102.0"/>' +
+                                                '</bpmndi:BPMNShape>' +
+                                            '</bpmndi:BPMNPlane>' +
+                                        '</bpmndi:BPMNDiagram>' +
+                                    '</bpmn:definitions>';
 
+                // 将字符串转换成图显示出来
                 this.bpmnModeler.importXML(bpmnXmlStr,  (err) => {
                     if (err) {
                         console.error(err);
@@ -209,10 +165,8 @@
                     console.log(event + ' on ' + e.element.id);
                 })
             }))
-
             // 创建一个新的模型
-            this.createNewDiagram(this.bpmnModeler);
-
+            this.createNewDiagram()
             // 根据元素监听事件
             let endEventNode = this.canvas.querySelector('g');
             endEventNode.addEventListener('click', (mouseEvent) => {
